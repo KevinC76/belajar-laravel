2ad23d7cec3c6351df1d61120392e9f393a6689e
KevinC76 <xckevinxc67@gmail.com>
2025-04-08 16:48:10 +0700

    create simple route and learn about route directory

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Blog Page</h1>
+    <div>
+        <div>
+            <h1>Judul 1</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, illo?</p>
+        </div>
+
+        <div>
+            <h1>Judul 1</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, illo?</p>
+        </div>
+    </div>
+</body>
+</html>
