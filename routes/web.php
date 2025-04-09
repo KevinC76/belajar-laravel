f2dd2459edbfbce0d1904f9fca883cee68898db5
KevinC76 <xckevinxc67@gmail.com>
2025-04-09 15:54:34 +0700

    blade templating engine (blade components)

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'kevin']);
+    return view('about', ['title' => 'About Page', 'name' => 'kevin chiputra']);
 });
 
 Route::get('/blog', function(){
-    return view('blog');
+    return view('blog', ['title' => 'Blog Page']);
 });
 
 Route::get('/contact', function(){
-    return view('contact');
+    return view('contact', ['title' => 'Contact Page']);
 });
