314e586bc11c2baee9d4994f242d91679f086be4
KevinC76 <xckevinxc67@gmail.com>
2025-04-11 17:21:47 +0700

    passing data ke component, wild card, static method pada laravel, @props

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,14 +12,7 @@
 </head>
 
 <body class="h-full">
-    <!--
-  This example requires updating your template:
 
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
         <x-navbar></x-navbar>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,8 +12,53 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page', 'name' => 'kevin chiputra']);
 });
 
-Route::get('/blog', function(){
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function(){
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul 1',
+            'author' => 'kevin c',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia velit modi, iusto
+            sint vero incidunt maxime
+            nam perspiciatis deserunt sit!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul 2',
+            'author' => 'kevin c',
+            'body' => 'sint vero incidunt maxime nam perspiciatis deserunt sit!'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul 1',
+            'author' => 'kevin c',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia velit modi, iusto
+            sint vero incidunt maxime
+            nam perspiciatis deserunt sit!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul 2',
+            'author' => 'kevin c',
+            'body' => 'sint vero incidunt maxime nam perspiciatis deserunt sit!'
+        ]
+        ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+
 });
 
 Route::get('/contact', function(){
