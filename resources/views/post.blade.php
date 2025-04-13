cc6b8b714a03f05a6125e2522208c6c7a53db938
KevinC76 <xckevinxc67@gmail.com>
2025-04-13 10:57:27 +0700

    fix: post category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,13 @@
 
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
 
-        <div class="text-base text-gray-500">
-            <a href="#">{{ $post->author->name }}</a> | {{ $post->created_at->format('j F Y') }} 2025
+        <div>
+            By
+            <a href="/authors/{{ $post->author->username }}"
+                class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a> | <a
+                href="/category/{{ $post->category->category_name }}"
+                class="hover:underline text-base text-gray-500">{{ $post->category->category_name }}</a> |
+            {{ $post->created_at->format('j F Y') }} 2025
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">Back to Posts &laquo;</a>
